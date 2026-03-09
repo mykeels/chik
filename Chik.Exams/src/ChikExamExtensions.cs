75d01ba79d59f6dbfbc435a3204e320575f7b2ad
mykeels <mykehell123@gmail.com>
2026-03-08 22:05:28 -0400

    Update package scripts and enhance service registrations
    - Added new API command in package.json for development environment.
    - Updated paths in aspirate deployment and email rendering scripts to reflect project structure changes.
    - Introduced UserSecretsId in Chik.Exams.csproj for managing sensitive information.
    - Enhanced service registration in ChikExamExtensions to include email and server error services, improving overall functionality.

--- Chik.Exams/src/ChikExamExtensions.cs
+++ Chik.Exams/src/ChikExamExtensions.cs
@@ -4,12 +4,18 @@ public static class ChikExamsExtensions
 {
     public static IServiceCollection AddChikExams(this IServiceCollection services, IConfiguration configuration)
     {
+        services.AddEmailService(
+            new(
+                configuration["EmailCredentials:Password"] ?? throw new Exception("EmailCredentials:Password is not set")
+            )
+        );
         services.AddAuditLog();
         services.AddExam();
         services.AddIpAddressLocation();
         services.AddLogin();
         services.AddQuiz();
         services.AddUser();
+        services.AddServerError();
         return services;
     }
 }
